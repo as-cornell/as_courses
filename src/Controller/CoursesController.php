e12c8c8acce258613cb6cf7ab2060a6b7a5e5fe2
markewilson <mark.e.wilson@gmail.com>
2025-09-02 13:12:24 -0400

    removes default semester variable set in controller for bare routes

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -14,7 +14,7 @@ class CoursesController extends ControllerBase {
       $defaultsemester = \Drupal::config("as_courses.defaults")->get("defaultsemester");
       if (!empty($defaultsemester)) {
         // if no semester is in path value from router is set to SP14, so go get current semester from settings 
-        if ($semester == 'SP24' OR empty($semester)){
+        if (empty($semester)){
             // set empty semester to current default semester
             $semester = $defaultsemester;
         }
